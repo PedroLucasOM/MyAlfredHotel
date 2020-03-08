cb948bb3ad54f40b97d81ece0fbf4b968b70ccb9
Pedro Lucas <pedro99lucasom@gmail.com>
2020-03-08 14:13:25 -0300

    Add validations

--- resource/hotel.py
+++ resource/hotel.py
@@ -2,30 +2,35 @@ from flask_restful import Resource, reqparse
 from werkzeug.exceptions import NotFound
 
 from model.hotel import HotelModel
+from utils.utils import non_empty_string
 
 
 def addArgs(arguments):
     arguments.add_argument('id')
-    arguments.add_argument('name')
-    arguments.add_argument('classification')
-    arguments.add_argument('cep')
-    arguments.add_argument('address')
-    arguments.add_argument('neighborhood')
-    arguments.add_argument('city')
-    arguments.add_argument('state')
+    arguments.add_argument('name', type=non_empty_string, required=True, nullable=False,
+                           help='The field Name cannot be left blank')
+    arguments.add_argument('classification', required=True, nullable=False, type=float,
+                           help='The field Classification cannot be null')
+    arguments.add_argument('cep', type=non_empty_string, required=True, nullable=False,
+                           help='The field CEP cannot be null')
+    arguments.add_argument('address', type=non_empty_string, required=True, nullable=False,
+                           help='The field Address cannot be left blank')
+    arguments.add_argument('neighborhood', type=non_empty_string, required=True, nullable=False,
+                           help='The field Neighborhood cannot be left blank')
+    arguments.add_argument('city', type=non_empty_string, required=True, nullable=False,
+                           help='The field City cannot be left blank')
+    arguments.add_argument('state', type=non_empty_string, required=True, nullable=False,
+                           help='The field State cannot be left blank')
 
 
 class HotelResourceAll(Resource):
-    arguments = reqparse.RequestParser()
+    arguments = reqparse.RequestParser(bundle_errors=True)
     addArgs(arguments)
 
     def get(self):
         return [hotel.json() for hotel in HotelModel.query.all()]
 
     def post(self):
-        arguments = reqparse.RequestParser()
-        addArgs(arguments)
-
         data = HotelResource.arguments.parse_args()
         hotel = HotelModel(**data)
         hotel.save()
@@ -33,7 +38,7 @@ class HotelResourceAll(Resource):
 
 
 class HotelResource(Resource):
-    arguments = reqparse.RequestParser()
+    arguments = reqparse.RequestParser(bundle_errors=True)
     addArgs(arguments)
 
     def get(self, id):
